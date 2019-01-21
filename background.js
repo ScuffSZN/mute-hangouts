0901c83891462aead0d35863c77caeb917a950c7
ScuffedDev <45866296+ScuffedDev@users.noreply.github.com>
2019-01-21 13:38:00 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Simple extension to replace lolcat images from
+// http://icanhascheezburger.com/ with loldog images instead.
+
+
+
+chrome.webRequest.onBeforeRequest.addListener(function(d){
+  return {cancel:true};
+},{urls:["*://*.gstatic.com/chat/sounds/*"]},
+  ["blocking"]);
