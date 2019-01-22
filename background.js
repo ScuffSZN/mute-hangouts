b0c3ffec322b80c4cf743bfbc452943a1010cd85
ScuffedDev <45866296+ScuffedDev@users.noreply.github.com>
2019-01-22 15:00:45 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Simple extension to replace lolcat images from
-// http://icanhascheezburger.com/ with loldog images instead.
-
-
+/*
+ * Created by @ScuffedDev
+ */
 
 chrome.webRequest.onBeforeRequest.addListener(function(d){
   return {cancel:true};
